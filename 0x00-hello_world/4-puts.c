9145361dd7ac947605dfb6e5bf143ffd53222508
Kenn-eth <royalmanbiz@gmail.com>
2022-09-11 09:07:11 -0700

    C scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h> 
+
+/**
+ *  * main - Entry point
+ *  * Description: C program that prints a function followed by a new line using put command
+ *  * Return: 0 if successful
+ *  */
+
  int main(void)
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 } 
 
