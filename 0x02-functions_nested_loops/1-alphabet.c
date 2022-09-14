0bb8951a89adc683f27be6a83468f5446401dd06
Kenn-eth <royalmanbiz@gmail.com>
2022-09-14 14:14:59 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* description */
+
+void print_alphabet(void)
+{
+	int ch;
+for ( ch = 'a' ; ch <= 'z' ; ch++ )
+putchar (ch);
+putchar ('\n');
+}
+
+	int main(void)
+{
+
+	   
+	    {
+		    print_alphabet();
+	    }
+	           
+	    return (0);
+}
