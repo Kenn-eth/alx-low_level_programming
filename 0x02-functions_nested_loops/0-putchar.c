f20ab4c6b9135bf06605c84d44211c5c5598a8f7
Kenn-eth <royalmanbiz@gmail.com>
2022-09-14 12:11:47 -0700

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * main- main prints _putchar
- *
+ * main - main prints _putchar
  * return: returns 0 (successful)
  */
 int main(void)
 {
-        write(1,"_putchar\n",11);
-	return (0);
+write(1, "_putchar\n", 11);
+return (0);
 }
